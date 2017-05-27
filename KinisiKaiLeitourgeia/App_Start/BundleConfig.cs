0b4344642eeed8d83bd335976a7fc94e1b71dafe
Vasilis Rigas <vrigas1@gmail.com>
2017-05-27 15:59:53 +0300

    KendoWorksFinally

--- KinisiKaiLeitourgeia/App_Start/BundleConfig.cs
+++ KinisiKaiLeitourgeia/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace KinisiKaiLeitourgeia
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -26,6 +28,15 @@ namespace KinisiKaiLeitourgeia
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                "~/Scripts/kendo/kendo.all.min.js",
+                // "~/Scripts/kendo/kendo.timezones.min.js", // uncomment if using the Scheduler
+                "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
+               "~/Content/kendo/kendo.common-bootstrap.min.css",
+               "~/Content/kendo/kendo.bootstrap.min.css"));
         }
     }
 }
---
+++ KinisiKaiLeitourgeia/Models/Appointments/Appointment.cs
@@ -0,0 +1,21 @@
+﻿using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KinisiKaiLeitourgeia.Models.Appointments
+{
+    public class Appointment : ISchedulerEvent
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsAllDay { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string StartTimezone { get; set; }
+        public string EndTimezone { get; set; }
+        public string RecurrenceRule { get; set; }
+        public string RecurrenceException { get; set; }
+    }
+}
